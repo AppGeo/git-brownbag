3f01fedfac6096b0d54269bc3b49b3d630846942
Calvin Metcalf <cmetcalf@appgeo.com>
2014-01-31 13:37:50 -0500

    first

---
+++ route.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var app = module.exports = require('express')();
+var request = require("request");
+
+app.get("/:lat/:lng", function (req, res) {
+  request("http://ctbb.betal1.appgeo.com/api/providers", {
+    json: true,
+    qs:{
+      lat: req.params.lat,
+      lng: req.params.lng
+    }
+  }, function (err, resp) {
+    if (err) {
+      res.send(404);
+    } else {
+      res.jsonp(resp.body);
+    }
+  });
+});
---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var express = require("express");
+var request = require("request");
+
+var app = express();
+
+//put me in latter
+app.use(express.compress());
+//after  you do cors-middleware.js
+app.use(require('./cors-middleware'));
+
+//first version
+app.get("/", function (req, res) {
+  request("http://ctbb.betal1.appgeo.com/api/providers", {
+    json: true,
+    qs:{
+      lat: 41.18078578212309,
+      lng: -73.51166725158691
+    }
+  }, function (err, resp) {
+    if (err) {
+      res.send(404);
+    } else {
+      res.jsonp(resp.body);
+    }
+  });
+});
+
+//second version
+app.use(require('./route'));
+
+app.listen(3000);
+console.log("app is listning on port 3000");
